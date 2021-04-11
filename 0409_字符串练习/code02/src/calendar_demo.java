6369e2d552c8b36271ebb060b37d908fefb4d5b3
azurewei <wei_wei_lan@163.com>
2021-04-11 14:24:24 +0800

    module package 添加

---
+++ 0409_字符串练习/code02/src/calendar_demo.java
@@ -0,0 +1,5 @@
+public class calendar_demo {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ 0409_字符串练习/code02/src/com/bigdata/java/helloWorld.java
@@ -0,0 +1,12 @@
+package com.bigdata.java;
+
+public class helloWorld {
+    public static void main(String[] args) {
+        System.out.println("big data backage jjj");
+//        System.out.println("big data backage jjj");
+
+    }
+
+    public void method(){
+    }
+}
